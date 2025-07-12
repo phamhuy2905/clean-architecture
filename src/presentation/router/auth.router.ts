e9210db19aa76f75a1cbe4494cab5fbff381cdb3
phamhuy2905 <huybmt2527000+1@gmail.com>
2025-07-13 02:06:42 +0700

    refactor: avoid duplicate bind method

--- src/presentation/router/auth.router.ts
+++ src/presentation/router/auth.router.ts
@@ -4,19 +4,20 @@ import { container } from '../../infrastructure/container/container';
 import { TYPES } from '../../infrastructure/container/types';
 import AuthController from '../controller/AuthController';
 import { loginUserSchema } from '../validation/auth/loginUserSchema';
+import { bindMethods } from '../../shared/helper/base';
 
-const authController = container.get<AuthController>(TYPES.AuthController);
+const authController = bindMethods(container.get<AuthController>(TYPES.AuthController));
 
 const authRouter: TAppRouter = {
     register: {
         method: 'POST',
         validator: registerUserSchema,
-        handler: authController.register.bind(authController),
+        handler: authController.register,
     },
     login: {
         method: 'POST',
         validator: loginUserSchema,
-        handler: authController.login.bind(authController),
+        handler: authController.login,
     },
 };
 
--- src/presentation/router/user.router.ts
+++ src/presentation/router/user.router.ts
@@ -2,13 +2,14 @@ import { TAppRouter } from '../../shared/common/types/base';
 import { container } from '../../infrastructure/container/container';
 import { TYPES } from '../../infrastructure/container/types';
 import UserController from '../controller/UserController';
+import { bindMethods } from '../../shared/helper/base';
 
-const userController = container.get<UserController>(TYPES.UserController);
+const userController = bindMethods(container.get<UserController>(TYPES.UserController));
 
 const userRouter: TAppRouter = {
     getProfile: {
         method: 'GET',
-        handler: userController.getProfile.bind(userController),
+        handler: userController.getProfile,
     },
 };
 
